994a3f0a543f08643b9247205b3a35bc717474f8
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 17:49:58 +0300

    Update HelloController.php

--- app/controllers/HelloController.php
+++ app/controllers/HelloController.php
@@ -18,6 +18,15 @@ class HelloController extends BaseController
 	{
 			return View::make('hello.blade', array('list'=>array(1,2,3,4,5), 'item' =>'my text', 'ival'=>100));
 	}
+	public function getAction()
+	{
+			return View::make('hello.my-form');
+	}
+	public function postMyForm()
+	{
+			return print_r(Input::all(),true);
+	}
+	
 
 
 
