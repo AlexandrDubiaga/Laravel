a93c796cde52f9159754e0dd27c3b62dca5a94f9
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-16 19:18:37 +0300

    Update Track.php

--- app/models/Track.php
+++ app/models/Track.php
@@ -3,6 +3,10 @@ class Track extends Eloquent
 {
   protected $table = 'Track';
   protected $primaryKey = 'TrackId';
+   protected function genre()
+    {
+        return $this->hasOne('Genre');
+    }
   
 }
 
