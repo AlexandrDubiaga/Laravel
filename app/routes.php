981f2ac29ce57dc3eb189e9affbe032404c737a5
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 20:40:37 +0300

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,24 +1,12 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
 {
-	return "Test string";
-}
-);
+	return Redirect::to('/artists');
+});
 
-Route::get('/hello/{name?}','HelloController@showIndex');
-Route::get('/form/','HelloController@showForm');
-Route::post('/form','HelloController@postForm');
-Route::get('/blade','HelloController@showBlade');
-Route::controller('test','HelloController');
+Route::get('/artists', 'WorkController@showArtists');
+
+Route::get('/artist/{id}', 'WorkController@getArtist');
+
+Route::get('/album/{id}', 'WorkController@getAlbum');
+
+Route::get('/track/{id}', 'WorkController@getTrack');
