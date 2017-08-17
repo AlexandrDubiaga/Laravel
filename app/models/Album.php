fa97093a0d215fa341880b58f2fca6955acdb31e
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 20:34:45 +0300

    Update Album.php

--- app/models/Album.php
+++ app/models/Album.php
@@ -1,12 +1,16 @@
 <?php
 class Album extends Eloquent
 {
-  protected $table = 'Album';
-  protected $primaryKey = 'AlbumId';
-   public function track()
+    protected $table = 'Album';
+    protected $primaryKey = 'AlbumId';
+
+    public function tracks()
     {
-        return $this->hasMany('Track','AlbumId','AlbumId');
+        return $this->hasMany('Track', 'AlbumId', 'AlbumId');
     }
-  
+
+    public function artist()
+    {
+        return $this->belongsTo('Artist', 'ArtistId', 'ArtistId');
+    }
 }
-?>
